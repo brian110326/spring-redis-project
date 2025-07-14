75e5265e97fc291496d5c79189098d880382833a
brian <brian110326@gmail.com>
2025-07-14 14:37:54 +0900

    first commit

---
+++ src/main/java/hello/redis/BoardService.java
@@ -0,0 +1,27 @@
+package hello.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    // Cacheable : Cache Aside 전략으로 캐싱이 적용됨
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size",
+            cacheManager = "boardCacheManager")
+    public List<Board> getBoards(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return pageOfBoards.getContent();
+    }
+
+}
